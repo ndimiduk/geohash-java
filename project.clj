f96215fdd4f902beb04c3fdce0ed244dafcecd7b
Nick Dimiduk <ndimiduk@gmail.com>
2012-04-18 20:13:33 -0700

    use lein to publish to clojars

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.clojars.ndimiduk/geohash-java "1.0.6"
+  :description "An implementation of Geohashes in pure Java."
+  :java-source-path "src/main/java"
+  :compile-path "target/classes"
+  :omit-source true)
